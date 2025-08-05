cf867215be8d44e977423298fbb5cd29ae20464f
Simon Lin <simonlin@google.com>
2025-08-05 23:40:29 +1000

    samples: button: improve LED control and clean up code formatting

--- samples/basic/button/src/main.c
+++ samples/basic/button/src/main.c
@@ -16,33 +16,38 @@
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
 
-#define SLEEP_TIME_MS	1
-
-/*
- * Get button configuration from the devicetree sw0 alias. This is mandatory.
- */
-#define SW0_NODE	DT_ALIAS(sw0)
+#define SW0_NODE DT_ALIAS(sw0)
 #if !DT_NODE_HAS_STATUS_OKAY(SW0_NODE)
 #error "Unsupported board: sw0 devicetree alias is not defined"
 #endif
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
-							      {0});
+
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
 static struct gpio_callback button_cb_data;
 
 /*
  * The led0 devicetree alias is optional. If present, we'll use it
  * to turn on the LED whenever the button is pressed.
  */
-static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios,
-						     {0});
+static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios, {0});
 
+/*
+ * Interrupt handler for button press and release.
+ * It reads the current button state and updates the LED accordingly.
+ */
 void button_pressed(const struct device *dev, struct gpio_callback *cb,
-		    uint32_t pins)
-{	int val = gpio_pin_get_dt(&button);
+                    uint32_t pins)
+{
+	int val = gpio_pin_get_dt(&button);
 	if (val > 0) {
 		printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+		if (led.port) {
+			gpio_pin_set_dt(&led, 1);
+		}
 	} else {
 		printk("Button released at %" PRIu32 "\n", k_cycle_get_32());
+		if (led.port) {
+			gpio_pin_set_dt(&led, 0);
+		}
 	}
 }
 
@@ -51,8 +56,7 @@ int main(void)
 	int ret;
 
 	if (!gpio_is_ready_dt(&button)) {
-		printk("Error: button device %s is not ready\n",
-		       button.port->name);
+		printk("Error: button device %s is not ready\n", button.port->name);
 		return 0;
 	}
 
@@ -63,11 +67,10 @@ int main(void)
 		return 0;
 	}
 
-	ret = gpio_pin_interrupt_configure_dt(&button,
-					      GPIO_INT_EDGE_BOTH);
+	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_BOTH);
 	if (ret != 0) {
 		printk("Error %d: failed to configure interrupt on %s pin %d\n",
-			ret, button.port->name, button.pin);
+		       ret, button.port->name, button.pin);
 		return 0;
 	}
 
@@ -81,7 +84,7 @@ int main(void)
 		led.port = NULL;
 	}
 	if (led.port) {
-		ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT);
+		ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
 		if (ret != 0) {
 			printk("Error %d: failed to configure LED device %s pin %d\n",
 			       ret, led.port->name, led.pin);
@@ -92,16 +95,11 @@ int main(void)
 	}
 
 	printk("Press the button\n");
-	if (led.port) {
-		while (1) {
-			/* If we have an LED, match its state to the button's. */
-			int val = gpio_pin_get_dt(&button);
 
-			if (val >= 0) {
-				gpio_pin_set_dt(&led, val);
-			}
-			k_msleep(SLEEP_TIME_MS);
-		}
+	/* Sleep forever; LED is controlled in ISR */
+	while (1) {
+		k_sleep(K_FOREVER);
 	}
+
 	return 0;
 }
